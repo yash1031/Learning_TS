555a63e116bb02b7f72545e4292f93e72eb9198c
yash1031 <yash735131@gmail.com>
2025-07-23 23:21:43 +0530

    First Commit

---
+++ intro.js
@@ -0,0 +1,26 @@
+function logpoint1(p) {
+    console.log(typeof p);
+}
+logpoint1("Ram");
+// type WindowStates = string | Number | Array;
+// type LockStates = "locked" | "unlocked";
+// type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+// var windowState: WindowStates= 1222222222222;
+// console.log(windowState)
+// type Username = string;
+// type Age = number;
+// type IsActive = boolean;
+// var name: Username = "Alice";
+// var age: Age = 25;
+// var active: IsActive = true;
+// interface User {
+//     name: string;
+//     id: number;
+//   }
+//   const user: User = {
+//     username: "Hayes",
+//     id: 0,
+//   }
+//  let user= { name: "Yash", age: 25};
+//  console.log("Yash");
+//  console.log(user.email);
---
+++ intro.ts
@@ -0,0 +1,35 @@
+function logpoint1(p: string | number) {
+    console.log(typeof p);
+  }
+
+logpoint1("Ram");
+
+
+type WindowStates = string | Number;
+type LockStates = "locked" | "unlocked";
+type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+var windowState: WindowStates= 1222222222222;
+console.log(windowState)
+
+// type Username = string;
+// type Age = number;
+// type IsActive = boolean;
+
+// var name: Username = "Alice";
+// var age: Age = 25;
+// var active: IsActive = true;
+
+// interface User {
+//     name: string;
+//     id: number;
+//   }
+   
+//   const user: User = {
+//     username: "Hayes",
+//     id: 0,
+//   }
+
+//  let user= { name: "Yash", age: 25};
+//  console.log("Yash");
+//  console.log(user.email);
